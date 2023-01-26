2421ceb32093966471a133f9a8da6ee0c6485587
Guilherme <guilhermefmk9@gmail.com>
2023-01-26 10:54:52 -0300

    week 1 finished

---
+++ homeworks/week1/hw_week_1_a.sql
@@ -0,0 +1,65 @@
+
+--QUESTION 3
+SELECT
+	COUNT(1)
+FROM 
+	green_taxi_trips 
+WHERE
+	DATE(lpep_dropoff_datetime) = '2019-01-15' AND
+	DATE(lpep_pickup_datetime) = '2019-01-15';
+
+--QUESTION 4
+SELECT 
+	lpep_pickup_datetime
+FROM 
+	green_taxi_trips g
+JOIN(
+	SELECT
+		MAX(trip_distance) AS max_trip
+	FROM 
+		green_taxi_trips) m
+ON
+	g.trip_distance = m.max_trip
+;
+
+--QUESTION 5
+
+SELECT
+	DATE(lpep_pickup_datetime),
+	passenger_count,
+	COUNT(passenger_count)
+FROM 
+	green_taxi_trips
+WHERE
+	DATE(lpep_pickup_datetime) = '2019-01-01' AND
+	passenger_count IN (2,3)
+GROUP BY
+	passenger_count,DATE(lpep_pickup_datetime);
+	
+--QUESTION 6
+
+SELECT
+	g."PULocationID",
+	zpu."Zone" embarque,
+	g."DOLocationID",
+	zdo."Zone" desembarque,
+	g.tip_amount
+FROM 
+	green_taxi_trips g
+JOIN zones zpu
+	ON	g."PULocationID" = zpu."LocationID"
+JOIN zones zdo
+	ON	g."DOLocationID" = zdo."LocationID"
+WHERE zpu."Zone" LIKE 'Astoria'
+ORDER BY g.tip_amount DESC
+LIMIT 1;
+
+
+
+
+	
+
+
+
+	
+
